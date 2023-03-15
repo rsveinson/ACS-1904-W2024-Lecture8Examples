7ffe0079b8cee7967beea6eae381aad1f5da8cf5
rsveinson <rsveinson@mts.net>
2023-03-15 12:00:47 -0500

    added some further demo code to test  new exception what happens when you don't try catch an operation that might throw an invalid sin execpetion

--- TestNewException.java
+++ TestNewException.java
@@ -4,7 +4,7 @@
  */
     public class TestNewException
 {
-    public static void main(String[] args)
+    public static void main(String[] args)throws InvalidSINException
     {
         try {
             // a valid SIN.
@@ -17,13 +17,21 @@
             System.out.println("\nAttempting to create employee 123456789");
             Employee e2 = new Employee("123456789", "Steve Stephens", "instructor");
             System.out.println("Create OK: "+e2);
-        }
-
+        }// end try
+        /* note that the custom message in the catch block
+         * refers to the getter in the checked exception object e
+         */
         catch (InvalidSINException e){
             System.out.println("Error creating employee. "+
                 "The provided SIN, "+ e.getSin()+" is invalid ");
+              System.out.println(e);
               e.printStackTrace();
         }
+        
+        // System.out.println("Attempting to create employee 046454286");
+        // Employee e3 = new Employee("0454286", "Keith Moon", "drummer");
+        // System.out.println("Create OK: "+e3);
+        
         
         System.out.println("program ended.");
     }
