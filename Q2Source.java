bfb0f1e27ec32e646743ae51ac75f5fefe33e71d
rsveinson <rsveinson@mts.net>
2025-03-10 08:58:52 -0500

    added a finally trap demo to Q2Source.java

--- Q2Source.java
+++ Q2Source.java
@@ -5,12 +5,28 @@ public class Q2Source
         int total = 0;
         Scanner kb = new Scanner(System.in);
         System.out.println("enter a number: ");
-        while (kb.hasNext()){
-            String number = kb.next();
-            int n = Integer.parseInt(number);  
+        String number = kb.next();
+        
+        while (!number.equals("q")){
+            
+            int n = Integer.parseInt(number); 
+            //int n = parseInput(number);
             total += n;
             System.out.println("enter a number: ");
+            number = kb.next();
         }
         System.out.println("total of numbers is "+total);
     }
+
+    public static int parseInput(String s){
+        try{
+            return Integer.parseInt(s);
+        }// end try
+        catch (NumberFormatException e){
+            System.out.println("oops, not an int");
+        }// end catch
+        finally{
+            return 0;
+        }// end finally
+    }//end parse input
 }
